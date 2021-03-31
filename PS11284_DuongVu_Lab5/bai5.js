1d536a0f328d69478f2592ba580e1fb25f900ce8
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-03-31 14:09:08 +0700

    Lab5 Bai5

--- PS11284_DuongVu_Lab5/bai5.js
+++ PS11284_DuongVu_Lab5/bai5.js
@@ -18,5 +18,38 @@ function showTime() {
     timeElement.innerHTML = html;
 }
 
+// Hàm tính tiền
+function tinhTien() {
+    var nguoiLon_arr = document.getElementsByClassName('nl');
+    var treEm_arr = document.getElementsByClassName('te');
+    var soTien_arr = document.getElementsByClassName('tien');
+
+    var tongNguoiLon = 0;
+    var tongTreEm = 0;
+    var tongTien = 0;
+
+    for(var i = 0; i < nguoiLon_arr.length; i++) {
+        var soNguoiLon = nguoiLon_arr[i].innerText;
+        var soTreEm = treEm_arr[i].innerText;
+        soNguoiLon = parseInt(soNguoiLon);
+        soTreEm = parseInt(soTreEm);
+
+        tongNguoiLon += soNguoiLon;
+        tongTreEm += soTreEm;
+
+        var total = soNguoiLon*1000000 + soTreEm*600000;
+        tongTien += total;
+        var formatTotal = (String(tongTien)).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        soTien_arr[i].innerText = formatTotal + ' VNĐ';
+    }
+
+    var formatTongTien = (String(tongTien)).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    document.getElementById('tongnl').innerText = tongNguoiLon;
+    document.getElementById('tongte').innerText = tongTreEm;
+    document.getElementById('tongtien').innerText = formatTongTien + ' VNĐ';
+
+}
+
 
 start();
