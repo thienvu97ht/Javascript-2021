6045c3e4abae0324a0c558748346c0a64d8b6ce2
thienvu97ht <thien.vu97ht123@gmail.com>
2021-04-14 21:15:35 +0700

    Lab7 bai 3 update1

--- PS11284_DuongVu_Lab7/Bai3/bai3.js
+++ PS11284_DuongVu_Lab7/Bai3/bai3.js
@@ -2,11 +2,24 @@
 var nameSpInput = document.querySelector('input[name="tensp"]');
 var soLuongInput = document.querySelector('input[name="soluong"]');
 var nsxInput = document.querySelector('select[name="nsx"]');
+var tinhTrangInput = document.querySelectorAll('input[name="tinhtrang"]');
+var mauSacInput = document.querySelectorAll('input[type="checkbox"]');
+var messageErr = document.querySelector('.form-message');
 
 // Điều kiện
 /* 
     2 <= nameSpInput.length <= 30
     1 < = parseInt(soLuongInput.value) <= 100
     nsxInput.value == 1, 2 ,3
+
     
 */
+
+function kiemTra() {
+    if (nameSpInput.value.length >= 2 && nameSpInput.value.length <= 30) {
+        undefined
+    } else {
+        messageErr.innerHTML = `Vui lòng nhập tên sản phẩm lớn 2 và ít hơn 30 ký tự`;
+        break;
+    }
+}
